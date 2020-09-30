82df5c442282b93f58bf580377db8fa5ef1ac0ea
iStad <70858110+iStad@users.noreply.github.com>
2020-09-30 21:03:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ exports.min = array => array.length && Math.min(...array);
 
 exports.max = array => array.length && Math.max(...array);
 
-exports.avg = function avg (array) {
+exports.avg = function avg (array = []) {
   const arrayLength = array.length;
 
   return arrayLength && array.reduce((s, el) => s + el) / arrayLength;
